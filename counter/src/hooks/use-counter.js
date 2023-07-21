b47c23a89732992482705e39dafdc6caae01af07
BhargaviMupparaju <105601164+BhargaviMupparaju@users.noreply.github.com>
2023-07-21 11:59:25 -0400

    Used the custom hook in forward and backward

--- counter/src/hooks/use-counter.js
+++ counter/src/hooks/use-counter.js
@@ -1,16 +1,22 @@
 // customs hooks helps to reuse the code. suppose here we are using the counter to increment and decrement.
 // to increase the reusablity we use the shared code in the hooks for forward and backward counter.
 import { useState, useEffect } from "react";
-const useCounter = () => {
+
+const useCounter = (forwards = true) => {
+	//Here default parameter we are using forward true . if parameter is false it will go to backward
 	const [counter, setCounter] = useState(0);
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			setCounter((prevCounter) => prevCounter + 1);
+			if (forwards) {
+				setCounter((prevCounter) => prevCounter + 1);
+			} else {
+				setCounter((prevCounter) => prevCounter - 1);
+			}
 		}, 1000);
 
 		return () => clearInterval(interval);
-	}, []);
+	}, [forwards]);          // since the useeffect runs based on the parameter we used the dependency
 
 	return counter;
 };
